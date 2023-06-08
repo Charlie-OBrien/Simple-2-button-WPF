f86d5c95e9e01cee14f3a1c8e5f6fe47ba6c351d
Charles O'Brien <charlesobriennb@gmail.com>
2023-06-07 22:13:13 -0300

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            bt1.Click += Bt1_Click;
+        }
+
+        private void Bt1_Click(object sender, RoutedEventArgs e)
+        {
+            textblock1.Text = "button 1 works";
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            label1.Content = "button 2 works";
+        }
+    }
+}
